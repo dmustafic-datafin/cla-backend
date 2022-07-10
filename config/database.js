96d243582e90f2e8eda05070047f65f2f7cce121
dmustafic-datafin <89752657+dmustafic-datafin@users.noreply.github.com>
2022-07-10 23:29:36 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'datafin.theworkpc.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'cla'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'pr0tekcij@'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
